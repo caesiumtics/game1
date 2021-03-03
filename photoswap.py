081e2ccc6f838124a6f6373b397afeaf536aba62
caesiumtics <79468579+caesiumtics@users.noreply.github.com>
2021-03-03 16:24:15 -0600

    Create photoswap.py

---
+++ photoswap.py
@@ -0,0 +1,25 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+
+# load and display an image with Matplotlib
+from matplotlib import image
+from matplotlib import pyplot
+
+"""
+# Simple Streamlit app by KS
+"""
+
+with st.echo(code_location = 'below'):
+    total_points = st.slider("Show image?", 0, 1, 0)
+    
+    # load image as pixel array
+    image = image.imread('kolala.jpeg')
+    # summarize shape of the pixel array
+    print(image.dtype)
+    print(image.shape)
+    # display the array of pixels as an image
+    pyplot.imshow(image)
+    pyplot.show()
