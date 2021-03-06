f13769bdfced57c974556ebfd2fedc366ed21890
caesiumtics <79468579+caesiumtics@users.noreply.github.com>
2021-03-05 21:12:39 -0600

    Update photoswap.py

--- photoswap.py
+++ photoswap.py
@@ -16,16 +16,7 @@ import matplotlib.pyplot as plt
 
 with st.echo(code_location = 'below'):
     total_points = st.slider("Show image?", 0, 100, 50)
-    
-    # load image as pixel array
-    image = image.imread('AE.jpg')
-    # summarize shape of the pixel array
-    print(image.dtype)
-    print(image.shape)
-    # display the array of pixels as an image
-    pyplot.imshow(image)
-    pyplot.show()
-    
+        
     fig = plt.figure()
 
     x = np.linspace(0, 1, 10)
