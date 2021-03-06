005ba234937e8d8add390a9fc112f5df12536ef1
caesiumtics <79468579+caesiumtics@users.noreply.github.com>
2021-03-05 21:12:04 -0600

    Update photoswap.py

--- photoswap.py
+++ photoswap.py
@@ -3,10 +3,12 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import numpy as np
 
 # load and display an image with Matplotlib
-from matplotlib import image
-from matplotlib import pyplot
+# from matplotlib import image
+# from matplotlib import pyplot
+import matplotlib.pyplot as plt
 
 """
 # Simple Streamlit app by KS
@@ -23,3 +25,12 @@ with st.echo(code_location = 'below'):
     # display the array of pixels as an image
     pyplot.imshow(image)
     pyplot.show()
+    
+    fig = plt.figure()
+
+    x = np.linspace(0, 1, 10)
+    y = x
+    
+    plt(x, y)
+
+    st.plotly_chart(fig)
